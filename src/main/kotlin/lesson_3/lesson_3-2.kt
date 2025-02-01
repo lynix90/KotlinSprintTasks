80fbdfb3ea41c87ac9f03cbd6417d907a56c8e99
v.guschin <lynix90@gmail.com>
2025-02-01 21:35:34 +0400

    KS-3-2 personal data view

---
+++ src/main/kotlin/lesson_3/lesson_3-2.kt
@@ -0,0 +1,13 @@
+package lesson_3
+
+fun main() {
+    val firstName = "Татьяна"
+    val oldSurname = "Андреева"
+    val patronymic = "Сергеевна"
+    val actualSurname = "Сидорова"
+    val ageBeforeMarried: Byte = 20
+    val ageAfterMarried: Byte = 22
+
+    println("$oldSurname $firstName $patronymic, $ageBeforeMarried")
+    println("$actualSurname $firstName $patronymic, $ageAfterMarried")
+}
