61c20eb18311548adc4888badc44fdafbdfe0631
v.guschin <lynix90@gmail.com>
2025-02-01 17:54:30 +0400

    Time in space calculating

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,13 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    val timeInSpace: Float = 6480F //или верни обратно инт и ок
+    val hoursPartOfTime = (timeInSpace / 3600) //целые часы
+    val minutesPartOfTime = (timeInSpace % 3600) / 60 //целые минуты
+    val secondsTimeOfTime = (timeInSpace % 3600) % 60
+    print(hoursPartOfTime)
+    print(":")
+    print(minutesPartOfTime)
+    print(":")
+    print(secondsTimeOfTime)
 }
