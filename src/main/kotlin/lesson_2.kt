66481fbe7c6c1b5df2e3860cd2e0feb2cfebad11
v.guschin <lynix90@gmail.com>
2025-02-01 19:14:54 +0400

    KS-2-1 average rating calculating

---
+++ src/main/kotlin/lesson_2.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val studentOneRating = 3.0
+    val studentTwoRating = 4.0
+    val studentThreeRating = 3.0
+    val studentFourRating = 5.0
+
+    println("среднее арифметическое = " + ((studentOneRating + studentTwoRating + studentThreeRating + studentFourRating) / 4))
+}
